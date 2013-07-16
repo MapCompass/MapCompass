720ddb7418b33497c547f06dc0228b85e306fef5
MapCompass <mrman916@gmail.com>
2013-07-16 07:18:52 -0700

    Updated database name

--- src/php/database.php
+++ src/php/database.php
@@ -4,7 +4,7 @@
     {
         die("Database connection failed cc: ".mysql_error());
     }
-    $db = mysql_select_db('MYF',$connection);
+    $db = mysql_select_db('MC',$connection);
     if(!$db)
     {
         die("Database connection faileddd : ".mysql_error());   
